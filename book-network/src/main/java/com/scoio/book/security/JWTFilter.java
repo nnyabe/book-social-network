1e885f3cbe0def22aba547ab151f6fb0ea8a64ab
nnyabe.chamamme <nnyabe.chamamme@amalitech.com>
2025-02-07 10:13:40 +0000

    Starting Out some security configurations

---
+++ book-network/src/main/java/com/scoio/book/security/JWTFilter.java
@@ -0,0 +1,46 @@
+package com.scoio.book.security;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import io.micrometer.common.lang.NonNull;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpHeaders;
+
+@Service
+@RequiredArgsConstructor
+public class JWTFilter extends OncePerRequestFilter {
+    private JWTService jwtService;
+
+    @Override
+    protected void doFilterInternal(
+        @NonNull HttpServletRequest request,
+        @NonNull HttpServletResponse response,
+        @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        if(request.getServletPath().contains("/api/v1/auth")){
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        final String jwt;
+        final String userEmail;
+
+        if(authHeader == null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        jwt = authHeader.substring(7);
+        userEmail jwtService.extractUsername(jwt);
+    }
+
+}
